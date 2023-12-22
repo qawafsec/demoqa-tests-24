a452eec0fbc5fb47889ed9caeacaad8bcbd63193
dtuchs <dtuchs@gmail.com>
2023-12-22 23:58:50 +0600

    added junit 5

--- src/test/java/guru/qa/SimpleJUnitTest.java
+++ src/test/java/guru/qa/SimpleJUnitTest.java
@@ -1,38 +1,43 @@
 package guru.qa;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SimpleJUnitTest {
 
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("###\n\n @BerforeAll\n");
-    }
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("\n\n### @BeforeAll\n");
+  }
 
-    @AfterAll
-    static void afterAll() {
-        System.out.println("### @AfterAll\n\n");
-    }
+  @AfterAll
+  static void afterAll() {
+    System.out.println("### @AfterAll\n\n");
+  }
 
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("###      @BerforeEach");
-    }
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("###     @BeforeEach");
+  }
 
-    @AfterEach
-    void afterEach() {
-        System.out.println("###      @AfterEach\n");
-    }
+  @AfterEach
+  void afterEach() {
+    System.out.println("###     @AfterEach\n");
+  }
 
+  @Test
+  void firstTest() {
+    System.out.println("###         @Test firstTest");
+    Assertions.assertTrue( 2 * 2 == 4);
+  }
 
-    @Test
-    void firstTest() {
-        System.out.println("###           @Test firstTest");
-        Assertions.assertTrue( 2 * 2 == 4);
-    }
-    @Test
-    void secondTest() {
-            System.out.println("###           @Test secondTest");
-            Assertions.assertTrue( 2 / 2 == 1);
-    }
+  @Test
+  void secondTest() {
+    System.out.println("###       @Test secondTest");
+    Assertions.assertTrue( 2 + 2 == 4);
+  }
 }
